3f91721dd88910d7a3f6b0f25b80965f48af8bf3
tjunjet <latyun142@gmail.com>
2022-07-10 11:18:14 +0800

    Added photo in the root

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import tjunjet_selfie from './assets/images/tjunjet_selfie.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={tjunjet_selfie} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
