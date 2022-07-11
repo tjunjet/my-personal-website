9247b51b3d4861a3cd1cc3f86bda5948186daddd
tjunjet <latyun142@gmail.com>
2022-07-12 01:46:01 +0800

    Used hash router

--- src/pages/root.js
+++ src/pages/root.js
@@ -17,29 +17,29 @@ export default function Root() {
       <div>
         <nav>
           <li><a href="/">Home</a></li>
-          <li><a href="about">About</a></li>
-          <li><a href="education">Education</a></li>
-          <li><a href="experience">Work Experience</a></li>
-          <li><a href="projects">Projects</a></li>
-          <li><a href="skills">Skills</a></li>
-          <li><a href="awards">Awards</a></li>
-          <li><a href="certifications">Certifications</a></li>
-          <li><a href="volunteering">Volunteering</a></li>
+          <li><a href="/about">About</a></li>
+          <li><a href="/education">Education</a></li>
+          <li><a href="/experience">Work Experience</a></li>
+          <li><a href="/projects">Projects</a></li>
+          <li><a href="/skills">Skills</a></li>
+          <li><a href="/awards">Awards</a></li>
+          <li><a href="/certifications">Certifications</a></li>
+          <li><a href="/volunteering">Volunteering</a></li>
         </nav>
         <section>
-          <HashRouter>
+          {/* <HashRouter> */}
             <Routes>
               <Route path = "/" element = {<Home />} />
-              <Route path = "about" element = {<About />} />
-              <Route path = "education" element = {<Education />} />
-              <Route path = "experience" element = {<Experience />} />
-              <Route path = "projects" element = {<Projects />} />
-              <Route path = "skills" element = {<Skills />} />
-              <Route path = "awards" element = {<Awards />} />
-              <Route path = "certifications" element = {<Certifications />} />
-              <Route path = "volunteering" element = {<Volunteering />} />
+              <Route path = "/about" element = {<About />} />
+              <Route path = "/education" element = {<Education />} />
+              <Route path = "/experience" element = {<Experience />} />
+              <Route path = "/projects" element = {<Projects />} />
+              <Route path = "/skills" element = {<Skills />} />
+              <Route path = "/awards" element = {<Awards />} />
+              <Route path = "/certifications" element = {<Certifications />} />
+              <Route path = "/volunteering" element = {<Volunteering />} />
             </Routes>
-          </HashRouter>
+          {/* </HashRouter> */}
         </section>
       </div>
     );
