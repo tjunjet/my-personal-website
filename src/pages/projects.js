447fbdf5a8e6bde48da4992886ff2d91721fc0c5
tjunjet <latyun142@gmail.com>
2022-07-11 18:04:15 +0800

    Added Project Decriptions

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -8,26 +8,105 @@ export default function Projects() {
                 <h1>PROJECTS</h1>
                 <table width="100%">
                     <hr></hr>
-                    {/* Robotics Intern at DSTA */}
+                    {/* Soldier Strong */}
                     <tr className = "Project-name">
-                        <td width="80%">Computer Vision Aided</td>
-                        <td className = "Date" width="20%">May 2022 - Aug 2022</td>
+                        <td width="80%">Fitness Tracker App "Soldier Strong" </td>
+                        <td className = "Date" width="20%">Jun 2022</td>
                     </tr>
                     <tr>
-                        <td className = "Job-scope">Defence Science and Technology Agency</td>
+                        <td className = "Job-scope">Champion (Best MVP), Brainhack CODE_EXP 2022</td>
                     </tr>
                     <tr>
                         <td className = "Description">
                             <br></br>
-                            <b>Awards and Recognitions:</b>
-                            <ul>
-                                <li>Spring 2022 Dean's List</li>
-                                <li>Fall 2021 Dean's List</li>
-                                <li>Electrical Lead of Blitz Booth - Champion, CMU Spring Carnival</li>
-                                <li>Vocal International Championship of Collegiate A Cappella (ICCA) Regional Runners-up</li>
-                                <li>Tartanhacks 2022 - Figma Award for Best Design</li>
-                                <li>Hack112 - Top 5 Teams (Honorable Mention)</li>
-                            </ul>
+                            Insert description here
+                            <br></br>
+                            <p><a href = "https://www.youtube.com/watch?v=ySV1-EBOtcU" className = "Youtube-Link">Youtube Video</a></p>
+                        </td>
+                    </tr>
+                    <hr></hr>
+                    {/* The Winter Soldier */}
+                    <tr className = "Project-name">
+                        <td width="80%"> Automated Snow-Shovelling Robot "The Winter Soldier"</td>
+                        <td className = "Date" width="20%"> Apr 2022 </td>
+                    </tr>
+                    <tr>
+                        <td className = "Job-scope"> Awarded Small Helpful Research Grant (SHRG), CMU Robotics Club</td>
+                    </tr>
+                    <tr>
+                        <td className = "Description">
+                            <br></br>
+                            Insert description here
+                        </td>
+                    </tr>
+                    <hr></hr>
+                    {/* Sleep Buddy */}
+                    <tr className = "Project-name">
+                        <td width="80%"> Sleep cycle recommender app "Sleep Buddy"</td>
+                        <td className = "Date" width="20%"> Jan 2022</td>
+                    </tr>
+                    <tr>
+                        <td className = "Job-scope"> Figma Award for Best Design, Tartanhacks 2022</td>
+                    </tr>
+                    <tr>
+                        <td className = "Description">
+                            <br></br>
+                            Insert description here
+                            <br></br>
+                            <p><a href = "https://www.youtube.com/watch?v=rizfjVbPnFM" className = "Youtube-Link">Youtube Video</a></p>
+                            <p><a href = "https://github.com/tjunjet/Sleep-Buddy" className = "Youtube-Link">GitHub Repository</a></p>
+                        </td>
+                    </tr>
+                    <hr></hr>
+                    {/* Winter Weather Guide */}
+                    <tr className = "Project-name">
+                        <td width="80%"> The Winter Wear-ther Guide </td>
+                        <td className = "Date" width="20%"> Dec 2021 </td>
+                    </tr>
+                    <tr>
+                        <td className = "Job-scope"> Insert name of Hackathon</td>
+                    </tr>
+                    <tr>
+                        <td className = "Description">
+                            <br></br>
+                            Insert description here
+                        </td>
+                    </tr>
+                    <hr></hr>
+                    {/* Geometronome Dash */}
+                    <tr className = "Project-name">
+                        <td width="80%"> Geometronome Dash </td>
+                        <td className = "Date" width="20%"> Nov 2021</td>
+                    </tr>
+                    <tr>
+                        <td className = "Job-scope"> </td>
+                    </tr>
+                    <tr>
+                        <td className = "Description">
+                            <br></br>
+                            Insert description here
+                            <br></br>
+                            <p><a href = "https://www.youtube.com/watch?v=FRZtIjSVagY" className = "Youtube-Link">Youtube Video</a></p>
+                            <p><a href = "https://github.com/tjunjet/112termproject" className = "Youtube-Link">GitHub Repository</a></p>
+                        </td>
+                    </tr>
+                    <hr></hr>
+                    {/* Hack112 */}
+                    <tr className = "Project-name">
+                        <td width="80%"> Motion-based Object Slicing Game</td>
+                        <td className = "Date" width="20%"> Oct 2021 </td>
+                    </tr>
+                    <tr>
+                        <td className = "Job-scope"> Top 5 Teams, Honorable Mention, CMU Hack112 Hackathon</td>
+                    </tr>
+                    <tr>
+                        <td className = "Description">
+                            <br></br>
+                            Insert description here
+                            <br></br>
+                            <p><a href = "https://www.youtube.com/watch?v=9WPAkc6xg0I" className = "Youtube-Link">Youtube Video</a></p>
+                            <p><a href = "https://github.com/tjunjet/Egg_and_Tofu" className = "Youtube-Link">GitHub Repository</a></p>
+
                         </td>
                     </tr>
                     <hr></hr>
