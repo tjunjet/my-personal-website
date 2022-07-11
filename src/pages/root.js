5045fc6f9f207e7a0b0dd0010cd1b4312f4cc308
tjunjet <latyun142@gmail.com>
2022-07-11 22:42:08 +0800

    New link

--- src/pages/root.js
+++ src/pages/root.js
@@ -17,14 +17,14 @@ export default function Root() {
       <div>
         <nav>
           <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/education">Education</a></li>
-          <li><a href="/experience">Work Experience</a></li>
-          <li><a href="/projects">Projects</a></li>
-          <li><a href="/skills">Skills</a></li>
-          <li><a href="/awards">Awards</a></li>
-          <li><a href="/certifications">Certifications</a></li>
-          <li><a href="/volunteering">Volunteering</a></li>
+          <li><a href="about">About</a></li>
+          <li><a href="education">Education</a></li>
+          <li><a href="experience">Work Experience</a></li>
+          <li><a href="projects">Projects</a></li>
+          <li><a href="skills">Skills</a></li>
+          <li><a href="awards">Awards</a></li>
+          <li><a href="certifications">Certifications</a></li>
+          <li><a href="volunteering">Volunteering</a></li>
         </nav>
         <section>
           <BrowserRouter>
