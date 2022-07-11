43a37d4a61ddc53bd5a07e16ec7c36a7a2b86e3a
tjunjet <latyun142@gmail.com>
2022-07-12 00:20:24 +0800

    Trying to resolve routing paths

--- src/pages/root.js
+++ src/pages/root.js
@@ -17,14 +17,14 @@ export default function Root() {
       <div>
         <nav>
           <li><a href="">Home</a></li>
-          <li><a href="about">About</a></li>
-          <li><a href="education">Education</a></li>
-          <li><a href="experience">Work Experience</a></li>
-          <li><a href="projects">Projects</a></li>
-          <li><a href="skills">Skills</a></li>
-          <li><a href="awards">Awards</a></li>
-          <li><a href="certifications">Certifications</a></li>
-          <li><a href="volunteering">Volunteering</a></li>
+          <li><a href="src/pages/about">About</a></li>
+          <li><a href="src/pages/education">Education</a></li>
+          <li><a href="src/pages/experience.js">Work Experience</a></li>
+          <li><a href="src/pages/projects">Projects</a></li>
+          <li><a href="src/pages/skills">Skills</a></li>
+          <li><a href="src/pages/awards">Awards</a></li>
+          <li><a href="src/pages/certifications">Certifications</a></li>
+          <li><a href="src/pages/volunteering">Volunteering</a></li>
         </nav>
         <section>
           <BrowserRouter>
