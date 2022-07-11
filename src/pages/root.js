f135067a62a8864e7582b9a9da247d3c45509aae
tjunjet <latyun142@gmail.com>
2022-07-12 01:44:06 +0800

    Used hash router

--- src/pages/root.js
+++ src/pages/root.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import {BrowserRouter, HashRouter, Route, Routes} from 'react-router-dom'
 
 import Home from './home'
 import About from './about'
@@ -17,29 +17,29 @@ export default function Root() {
       <div>
         <nav>
           <li><a href="/">Home</a></li>
-          <li><a href="pages/about.js">About</a></li>
-          <li><a href="pages/education.js">Education</a></li>
-          <li><a href="pages/experience.js">Work Experience</a></li>
-          <li><a href="pages/projects.js">Projects</a></li>
-          <li><a href="pages/skills.js">Skills</a></li>
-          <li><a href="pages/awards.js">Awards</a></li>
-          <li><a href="pages/certifications.js">Certifications</a></li>
-          <li><a href="pages/volunteering.js">Volunteering</a></li>
+          <li><a href="about">About</a></li>
+          <li><a href="education">Education</a></li>
+          <li><a href="experience">Work Experience</a></li>
+          <li><a href="projects">Projects</a></li>
+          <li><a href="skills">Skills</a></li>
+          <li><a href="awards">Awards</a></li>
+          <li><a href="certifications">Certifications</a></li>
+          <li><a href="volunteering">Volunteering</a></li>
         </nav>
         <section>
-          <BrowserRouter>
+          <HashRouter>
             <Routes>
               <Route path = "/" element = {<Home />} />
-              <Route path = "pages/about.js" element = {<About />} />
-              <Route path = "pages/education.js" element = {<Education />} />
-              <Route path = "pages/experience.js" element = {<Experience />} />
-              <Route path = "pages/projects.js" element = {<Projects />} />
-              <Route path = "pages/skills.js" element = {<Skills />} />
-              <Route path = "pages/awards.js" element = {<Awards />} />
-              <Route path = "pages/certifications.js" element = {<Certifications />} />
-              <Route path = "pages/volunteering.js" element = {<Volunteering />} />
+              <Route path = "about" element = {<About />} />
+              <Route path = "education" element = {<Education />} />
+              <Route path = "experience" element = {<Experience />} />
+              <Route path = "projects" element = {<Projects />} />
+              <Route path = "skills" element = {<Skills />} />
+              <Route path = "awards" element = {<Awards />} />
+              <Route path = "certifications" element = {<Certifications />} />
+              <Route path = "volunteering" element = {<Volunteering />} />
             </Routes>
-          </BrowserRouter>
+          </HashRouter>
         </section>
       </div>
     );
